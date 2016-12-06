ab4b7e7a4f28a5d7df0895964c2a2e6af40b56c4
kleveland <kacey.job@gmail.com>
2016-12-06 11:11:16 -0500

    n/a

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: "youtube_platinum"
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: "youtube_platinum"
 });
 
 exports.findOrCreate = function (prof, callback) {
