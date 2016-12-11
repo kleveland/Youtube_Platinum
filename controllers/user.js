81b8d02c2ab4d42ff2a4ca59b09ce5abae6d4477
kleveland <kacey.job@gmail.com>
2016-12-11 17:18:25 -0500

    backend stuff for updating user info

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
+
+
 module.exports = function (app, passport) {
+	var User = require('../models/user');
 	app.get('/login', function (req, res) {
 		console.log("LOGIN PAGE");
 		if (req.session.error) {
@@ -13,9 +16,13 @@ module.exports = function (app, passport) {
 		}
 	});
 
-	app.get('/userinfo'), function(req,res) {
+	app.get('/userinfo', function(req,res) {
 		res.send(req.user);
-	}
+	});
+
+	app.post('/userinfo/update', function(req,res) {
+		User.updateUser(req.user.id,req.body);
+	})
 
 	app.get('/logout', function (req, res) {
 		req.logout();
