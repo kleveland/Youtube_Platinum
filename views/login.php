dac7a75d3f64a2d13f46e1b8d0341a45440b8a77
nguyeneer <nguyeneer@gmail.com>
2016-12-11 23:15:59 -0500

    Added About Us Button Josh

--- views/login.php
+++ views/login.php
@@ -15,37 +15,57 @@
 	<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="assets/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
 	<link href="assets/css/main.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-	<!-- HEADER CONTAINER -->
-	<div class="header-container">
-		<!-- INFO OVERLAY -->
-		<div class="info-overlay">
-			<!-- MIDDLE TEXT -->
-			<div class="row-fluid info-middle">
-				<h1 class="text-center text-large uppercase text-white" style="padding-bottom: 20px;font-weight:100;">
+    <!-- HEADER CONTAINER -->
+    <div class="header-container">
+        <!-- INFO OVERLAY -->
+        <div class="info-overlay">
+            <!-- MIDDLE TEXT -->
+            <div class="row-fluid info-middle">
+                <h1 class="text-center text-large uppercase text-white" style="padding-bottom: 20px;font-weight:100;">
 					YouTube Platinum
 				</h1>
-				<?php
+                <?php
 					if(!empty($_GET['error'])) {
 						echo '<div class="alert alert-danger">' .
 							'<strong>Oops!</strong>' . $_GET['error'] .
 						'</div>';
 					}
-				?>
-				<a class="button btn-danger login-btn" href="/auth/google">Login With Google</a>
-			</div>
-			<!-- END OF MIDDLE TEXT -->
-		</div>
-		<!-- END OF INFO OVERLAY -->
-	</div>
-	<!-- END OF HEADER -->
+				?> <a class="button btn-danger login-btn" href="/auth/google">Login With Google</a>
 
-	<!-- JAVASCRIPT FILES -->
-	<script type="text/javascript" src="http://www.youtube.com/player_api"></script>
-	<script type="text/javascript" src="assets/plugins/js/jquery-2.1.4.min.js"></script>
-	<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+
+                            <div id="navthing">
+                                <h2><a href="#" id="loginform">About Us</a></h2>
+                                <div class="login">
+                                    <div class="arrow-up"></div>
+                                    <div class="formholder">
+                                        <div class="randompad">
+                                            <fieldset>
+                                                <p>This is how you use YoutubePlatium</p>
+                                            </fieldset>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+            </div>
+            <!-- END OF MIDDLE TEXT -->
+        </div>
+        <!-- END OF INFO OVERLAY -->
+    </div>
+    <!-- END OF HEADER -->
+    <!-- JAVASCRIPT FILES -->
+    <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
+    <script type="text/javascript" src="assets/plugins/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
 </body>
 
 </html>
